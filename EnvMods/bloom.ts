ec6801f2e9a1ca648ce31929f5c7da5dac766d50
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-09-02 14:18:03 -0700

    Add files via upload

---
+++ EnvMods/bloom.ts
@@ -0,0 +1,12 @@
+import * as remapper from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
+/**
+ * @param x
+ * @param y
+ * @param z
+ * @param sx
+ * @param sy
+ * @param sz
+ * @author splashcard__
+ */
+
+
---
+++ EnvMods/index.ts
@@ -0,0 +1,9 @@
+/*
+EnvMods- Something that sound official ig
+oh yea splashcard__ made this ;)
+*/
+
+export * from './bloom.ts'
+export * from './clouds.ts'
+export * from './despawn.ts'
+export * from './sun.ts'
