dec54cea29c3f0db54fb2d659b08ee71af0a025e
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-09-03 08:52:24 -0700

    Update despawn.ts

--- EnvMods/despawn.ts
+++ EnvMods/despawn.ts
@@ -1,36 +1,31 @@
 
 import * as remapper from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
-//really dumb way of making bts environment despawn
+//I forgor how to make cases work so we have this inneficeint idiot DONT BLAME ME I TRIED MY BEST OK
+
 /**
  * @param environment
  * @author splashcard__
  */
 
-export function despawn(enviro: string) {
-
-    enviro = "bts"
-    switch(enviro) {
-        case "bts":
-            enviro = "BTSEnvironment.[0]Environment"
-        case "bigMirror":
-            enviro = "BigMirrorEnvironment.[0]Environment"
-        case "billie":
-            enviro = "BillieEnvironment.[0]Environment"
-        case "crabRave":
-            enviro = "CrabRaveEnvironment.[0]Environment"
-        case "default":
-            enviro = "DefaultEnvironment.[0]Environment"
-        case "dragons":
-            enviro = "DragonsEnvironment.[0]Environment"
-        case "fallOutBoy":
-            enviro = "PyroEnvironment.[0]Environment"
-        case "gaga":
-            enviro = "GagaEnvironment.[0]Environment"
-        case "spooky":
-            enviro = "HalloweenEnvironment.[0]Environment"
+export function despawn(environment: string) {
+    
+    if(environment === "bts"){
+        const env = new remapper.Environment("BTSEnvironment.[0]Environment", "Contains")
+        env.position = [-69420, -69420, -69420];
+        env.push();
     }
 
-    const env = new remapper.Environment((enviro), "Contains")
-    env.position = [-69420, -69420, -69420];
-    env.push();
+    if (environment === "billie"){
+            const env = new remapper.Environment("BillieEnvironment.[0]Environment", "Contains")
+            env.position = [-69420, -69420, -69420];
+            env.push();
+        }
+
+    
+    if (environment === "spooky"){
+        const env = new remapper.Environment("BillieEnvironment.[0]Environment", "Contains")
+        env.position = [-69420, -69420, -69420];
+        env.push();
+    }
 }
+
