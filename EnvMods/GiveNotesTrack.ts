09855bf21c7465bfa976f66c92e1b7d7dc27d088
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-09-02 17:32:58 -0700

    Update GiveNotesTrack.ts

--- EnvMods/GiveNotesTrack.ts
+++ EnvMods/GiveNotesTrack.ts
@@ -1,14 +1,23 @@
 import * as remapper from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
 
 /**
+ * @param startx
+ * @param starty
+ * @param startz
  * @param time
+ * @param endx
+ * @param endy
+ * @param endz
  * @param dur
- * @param track
  * @author splashcard__
- */
+*/
 
-export function noteTrack(time: number, dur: number, track: string) {
+export function PlayerMove(time: number, dur: number, startX: number, endX: number, startY: number, endY: number, startZ: number, endZ: number) {
+    let event = new remapper.CustomEvent(time).remapper.animateTrack("track", dur);
+    event.animate.position = [[startX, startY, startZ, 0], [endX, endY, endZ, 1]];
+    event.push();
+    new remapper.CustomEvent(time).remapper.assignPlayerToTrack("track").push();
     remapper.notesBetween(time, dur, (note) => {
-        note.track = track
-    });
+        note.track = "track"
+    })
 }
