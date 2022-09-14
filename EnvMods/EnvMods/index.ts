04046ae5ff67df63601770bb7e603fbf1f46e265
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-09-13 21:45:37 -0700

    Update despawn.ts

---
+++ EnvMods/EnvMods/index.ts
@@ -0,0 +1,11 @@
+/*
+EnvMods- Something that sound official ig
+oh yea splashcard__ made this ;)
+*/
+
+export * from './clouds.ts'
+export * from './despawn.ts'
+export * from './sun.ts'
+export * from './water.ts'
+export * from './animatePlayer.ts'
+export * from './giveNotesTrack.ts'
