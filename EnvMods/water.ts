4a5d48c0bd2fa71efd82b3d8e0b0d49cd08c05a7
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-09-20 17:55:03 -0700

    Update water.ts

--- EnvMods/water.ts
+++ EnvMods/water.ts
@@ -7,10 +7,12 @@ import * as remapper from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
  */
 
 export function water(time: number, dur: number, y: number) {
-    const wall = new remapper.Wall(time, dur, 1, 0, 0) ;
-    wall.color = [0.04, 1.9, 2.3, 10];
-    wall.fake = true;
-    wall.interactable = false;
-    wall.scale = [4000, 0.5, 4000];
-    wall.animate.definitePosition = [[-2000, y, -2000, 0], [-2000, y, -2000, 1]]
+    const wall = new remapper.Wall(time, dur, 1, 0, 0);
+        wall.color = [0, 1.2, 3, 11.3],
+        wall.fake = true;
+        wall.interactable = false;
+        wall.scale = [5000, 0.5, 25000],
+        wall.animate.definitePosition = [[-2000, y, -2000, 0], [-2000, y, -2000, 1]]
+        
+    wall.push();
 }
