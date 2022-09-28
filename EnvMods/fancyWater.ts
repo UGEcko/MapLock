9b9fa1d03bdad71881ea4cacceaa375bb6081e07
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-09-28 10:16:22 -0700

    Update fancyWater.ts

--- EnvMods/fancyWater.ts
+++ EnvMods/fancyWater.ts
@@ -1,8 +1,7 @@
 import * as remapper from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
 
 export function fancyWater(y: number, uses: number) {
-    const env = new remapper.Environment("Regex")
-    env.id = new remapper.Regex("PlayersPlace").separate().add("Mirror").end(),
+    const env = new remapper.Environment(ENV.UNIVERSALL.MIRROR, "Regex")
     env.scale = [3000, 0.1, 3000];
     env.position = [-1000, y, -1000];
     env.duplicate = uses;
