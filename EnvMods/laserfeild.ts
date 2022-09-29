cfe9620619e1af6cf39bd5734e02cd24904de2fe
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-09-29 15:17:53 -0700

    Update laserfeild.ts

--- EnvMods/laserfeild.ts
+++ EnvMods/laserfeild.ts
@@ -1,13 +1,16 @@
 /**
  * @param y
+ * @param IDmin
+ * @param ammount
 */
 
 import * as remapper from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
-export function laserfeild (y) {
+export function laserfeild (y: number, IDmin: number, ammount:number) {
   const geo = new remapper.Geometry("Cube", {
     _shader: "Opaque_Light"
   }
-  geo.duplicate = 20;
+  geo.duplicate = ammount;
   geo.position = [(math.random() * (40)) - 20, y, (Math.random() * (40))  - 20];
+  geo.lightID = IDmin;
   geo.push();
 }
