eda979d6e07adafdb721e88856f370506b2b5528
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-04 15:21:44 -0700

    Update filterNotesTrack.ts

--- EnvMods/filterNotesTrack.ts
+++ EnvMods/filterNotesTrack.ts
@@ -1,10 +1,10 @@
 import * as remapper from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
 
 /**
-* @param time
-* @param dur
-* @param track
-* @author splashcard__
+* @param time time the track for the filtered notes will start
+* @param dur duration of the track for the filtered notes
+* @param track the name of the track for the filtered notes
+* @author splashcard__  its literally 2 in the morning :D
 */
 
 export function filterNotesTrack(lineLayer: number, lineIndex: number, time: number, duration: number, position: any, rotation: any, dissolve: any) {
