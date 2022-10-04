90e9a5e14d6805531bac0d268b9b9915e0aa10e2
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-04 15:17:17 -0700

    Update clouds.ts

--- EnvMods/clouds.ts
+++ EnvMods/clouds.ts
@@ -1,14 +1,13 @@
 import * as remapper from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
 
 /**
- * @param x
- * @param y
- * @param z
- * @param scalex
- * @param scaley
- * @param scalez
- * @param uses
- * @author splashcard__
+ * @param x the position on the x axis of the clouds
+ * @param y the position on the y axis of the clouds
+ * @param z the position on the z axis of the clouds
+ * @param scalex the scale on the x axis of the clouds
+ * @param scaley scalex the scale on the y axis of the clouds
+ * @param scalez scalex the scale on the z axis of the clouds
+ * @author splashcard__ <-- no way!!
  */
 
 export function clouds(x: number, y: number, z: number, scalex: number, scaley: number, scalez: number) {
