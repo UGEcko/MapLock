618c4078d9cc3f8e2fae8d18930fd5c85968f6e4
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-04 15:20:18 -0700

    Update fancyWater.ts

--- EnvMods/fancyWater.ts
+++ EnvMods/fancyWater.ts
@@ -1,5 +1,11 @@
 import * as remapper from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
 
+
+/**
+ *@param height the heght at which to spawn the water
+ *@author splashcard__ i should be studying right now
+*/
+
 export function fancyWater(posY: number) {
     const env = new remapper.Environment(ENV.UNIVERSALL.MIRROR, "Regex")
     env.scale = [3000, 0.1, 3000];
