4894256c85618b893ac2f1b74f779fa3b6dc5e09
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-05 12:18:31 -0700

    Update laserfeild.ts

--- EnvMods/laserfeild.ts
+++ EnvMods/laserfeild.ts
@@ -1,17 +1,29 @@
 import * as remapper from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
 
 /**
- * @param y
- * @param IDmin
- * @param ammount
+ * @param height the height to spawn the lasers at
+ * @param IDmin the minimum lightID for the first laser
+ * @param ammount the ammount of lasers to be spawned
+ * @param allowRotation allows rotation of the lasers on the x and z axis
+ * @author splashcard__ this one is pretty cool
 */
 
-export function laserfeild (y: number, IDmin: number, ammount:number) {
-  const geo = new remapper.Geometry("Cube", {
-    _shader: "Opaque_Light"
+export function laserfeild(height: number, IDmin: number, ammount:number, allowRotation: boolean) {
+  for(let i = 1; i < ammount; i++) {
+    const x = (Math.random() * (40)) - 20
+    const z = (Math.random() * (40)) - 20
+    const rot = (Math.random() * (40)) - 20
+    const rot2 = (Math.random() * (40)) - 20
+    const geo = new remapper.Geometry("Cube", {
+      _shader: "Opaque_Light"
+    })
+    geo.position = [x, height, z];
+    if (allowRotation = true) {
+      geo.rotation = [rot, 0, rot2];
+    }
+    else {
+      geo.rotation = [0, 0, 0]
+    }
+    geo.push();
   }
-  geo.duplicate = ammount;
-  geo.position = [(math.random() * (40)) - 20, y, (Math.random() * (40))  - 20];
-  geo.lightID = IDmin;
-  geo.push();
 }
