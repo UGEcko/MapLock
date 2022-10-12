0ad5a59bee1adde120aab99022e526141432bf0c
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-12 14:16:32 -0700

    Update animatePlayer.ts

--- EnvMods/animatePlayer.ts
+++ EnvMods/animatePlayer.ts
@@ -1,20 +1,15 @@
 import * as remapper from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
 
 /**
- * @param startx starting position of the track on the x axis
- * @param starty starting position of the track on the y axis
- * @param startz starting position of the track on the z axis
- * @param endx the ending position of the track on the x axis
- * @param endy the ending position of the track on the y axis
- * @param endz the ending position of the track on the z axis
+ * @param position the position of the animate Track CAN USE MULTIPLE POINTS
  * @param time the time at which the track will start
  * @param dur the duration of the track
  * @author splashcard__ thats me :pog:
 */
 
-export function playerMove(time: number, dur: number, startX: number, endX: number, startY: number, endY: number, startZ: number, endZ: number) {
+export function playerMove(time: number, dur: number, position: any) {
     const event = new remapper.CustomEvent(time).animateTrack("track", dur);
-    event.animate.position = [[startX, startY, startZ, 0], [endX, endY, endZ, 1]];
+    event.animate.position = position;
     event.push();
     new remapper.CustomEvent(time).assignTrackParent(["notes", "player"], "track")
     new remapper.CustomEvent(time).assignPlayerToTrack("player").push();
