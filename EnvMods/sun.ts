f7b7a9dafd0eb51aae36e5f5a25a8d208eb65be4
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-12 20:15:00 -0700

    Update sun.ts

--- EnvMods/sun.ts
+++ EnvMods/sun.ts
@@ -1,17 +1,15 @@
 import * as remapper from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
 
 /**
- * @param posX the position of the sun on the x axis
- * @param posY the position of the sun on the y axis
- * @param posZ the position of the sun on the z axis
+ * @param postion position to spawn the sun
  * @param scale the scale of the sun
  * @param lightID the ID to light the sun
  * @author splashcard__ respect the grind
  */
 
-export function sun(posX: number, posY: number, posZ: number, scale: number, lightID : number) {
+export function sun(position: any scale: number, lightID : number) {
     const sun = new remapper.Environment("GlowLineH$", "Regex")
-    sun.position = [posX, posY, posZ];
+    sun.position = position
     sun.scale = [scale, scale + 0.5, scale];
     sun.lightID = lightID;
     sun.duplicate = 1;
