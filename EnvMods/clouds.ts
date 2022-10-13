1ff23ebdcd8c1e8a6d5edc405ca92cadf554d729
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-13 15:19:18 -0700

    I definitely tested all of these👀

--- EnvMods/clouds.ts
+++ EnvMods/clouds.ts
@@ -4,7 +4,7 @@ import * as remapper from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
  * @param position the position to spawn the clouds at
  * @param rotation the rotation to spawn the clouds with
  * @param scale the scale to spwan the clouds with
- * @author splashcard__ <-- no way!!
+ * @author splashcard__ <-- no way!! 
  */
 
 export function clouds(position: any, scale: any, rotation: any) {
