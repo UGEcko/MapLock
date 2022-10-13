ce003609ca72e428d72965280193ad2cd44206b9
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-13 12:36:11 -0700

    Update laserfeild_class.ts

--- EnvMods/laserfeild_class.ts
+++ EnvMods/laserfeild_class.ts
@@ -1,13 +1,13 @@
-import * as rm from "https://deno.land/x/remapper/mod.ts"
-import * as envmods from './EnvMods/index.ts'
+import * as rm from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
 
-export type IDmin = number
-export type height = number
-export type allowRotation = boolean
-export type ammount = number
+/**
+* @param {number} IDmin
+* @param {number} height
+* @param {boolean} allowRotation
+*/
 
 export class lasefeild{
-  constructor(IDmin?: number, ammount: number, height?: number, allowrotation?: number) {
+  constructor(ammount: number) {
     for(let i = 1;  i > ammount; i++) {
         const x = (Math.random() * (40)) - 20
         const z = (Math.random() * (40)) - 20
@@ -18,10 +18,10 @@ export class lasefeild{
       })
       geo.lightID = IDmin;
       geo.position = [x, height, z]
+      geo.rotation = [rot, 0, rot2]
       if(allowrotation === false) {
         geo.rotation = [0, 0, 0]
       }
-      geo.rotation = [rot, 0, rot2]
       geo.push();
 
       get(IDmin) {return this.IDmin}
