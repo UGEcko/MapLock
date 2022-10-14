af4124fe88cd8c61f25009e5d46d0b6f4d7fc063
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-14 07:24:34 -0700

    Update EnvMods_setup.ts

--- setup/EnvMods_setup.ts
+++ setup/EnvMods_setup.ts
@@ -65,7 +65,7 @@ try {
 await Deno.stat(templatePath)
 
 // now copy to path
-const ignoredFiles = ["setup", ".git", "README.md", ".gitignore", "Examples.md", "test"]
+const ignoredFiles = ["setup", ".git", "README.md", ".gitignore", "Examples", "test", "LICENSE"]
 
 const tasks: Promise<void>[] = []
 
