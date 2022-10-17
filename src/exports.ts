fb3c82f4c0fb5c95370abd6769ae663f3cac9565
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-17 15:58:06 -0700

    Update exports for organization

--- src/exports.ts
+++ src/exports.ts
@@ -1,8 +1,3 @@
-/*
-MapLock- Something that sounds official ig
-oh yea splashcard__ made this ;)
-*/
-
 export * from './Environments/clouds.ts'
 export * from './Environments/despawn.ts'
 export * from './Environments/sun.ts'
