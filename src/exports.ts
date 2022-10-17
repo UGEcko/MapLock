cb37f8baaf4396a7ee102720c6ac8d4815d3e070
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-17 15:02:56 -0700

    Update exports.ts

--- src/exports.ts
+++ src/exports.ts
@@ -1,13 +1,13 @@
 /*
-EnvMods- Something that sound official ig
+MapLock- Something that sounds official ig
 oh yea splashcard__ made this ;)
 */
 
-export * from './clouds.ts'
-export * from './despawn.ts'
-export * from './sun.ts'
-export * from './water.ts'
-export * from './animatePlayer.ts'
-export * from './fancyWater.ts'
-export * from './laserfeild.ts'
-export * from './noteFilter.ts'
+export * from './Environments/clouds.ts'
+export * from './Environments/despawn.ts'
+export * from './Environments/sun.ts'
+export * from './general/animatePlayer.ts'
+export * from './general/fancyWater.ts'
+export * from './Environments/laserfeild.ts'
+export * from './general/noteFilter.ts'
+export * from './objects/stars.ts'
