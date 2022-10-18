8c037aaa1fc93c982f2dfee7d91074c439180f74
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-17 20:51:47 -0700

    fixed star color

--- src/objects/stars.ts
+++ src/objects/stars.ts
@@ -15,7 +15,7 @@ export function stars(amount: number, individualSize: number, Distance: number, 
       const starRotation: rm.Vec3 = [rm.rand(10, 360), rm.rand(10, 360), 0];
     
       stars.animate.definitePosition = rm.arrAdd(rm.rotatePoint(starRotation, [0, Distance, -individualSize / 2]), 0) as rm.Vec3;
-      stars.animate.color = [1,1,1,1];
+      stars.color = [1,1,1,1];
       stars.scale = [individualSize, individualSize, individualSize];
       stars.fake = true;
       stars.interactable = false;
