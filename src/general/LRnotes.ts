5e25aeed121329b0c75b1ab848fc6833a364f30b
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-18 13:21:57 -0700

    Note entry function :0

---
+++ src/general/LRnotes.ts
@@ -0,0 +1,19 @@
+import * as rm from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
+
+/**
+* @param time the time for the animation
+* @param duration the duration for the animation
+* @param leftNoteData the custom data for the left notes
+* @param rightNoteData the custom data for the right notes
+* @author splashcard__
+*/
+
+export function LRnotes(time, duration, leftNoteData, rightNoteData) {
+    rm.notesBetween(time, duration, (note) => {
+      if(note.position === [0, 0] || [0, 1] || [0, 2] || [1, 0] || [1, 1] || [1, 2]) {
+       note.customData = leftNoteData;
+    }
+      if(note.position === [2, 0] || [2, 1] || [2, 2] || [3, 0] || [3, 1] || [3, 2]) {
+       note.customData = rightNoteData;
+  }
+}
