74c237f61de8d5043c987107cf4dfdc3b6804b16
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-19 13:42:30 -0700

    Update fancyWater.ts

--- src/general/fancyWater.ts
+++ src/general/fancyWater.ts
@@ -1,12 +1,12 @@
-import * as remapper from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
-
+// deno-lint-ignore-file no-explicit-any
+import * as rm from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
 
 /**
  *@param height the heght at which to spawn the water
  *@author splashcard__ i should be studying right now
 */
 
-export function fancyWater(posY: number) {
+export function fancyWater(posY: number, pushMirrorQuality: boolean) {
     const env = new remapper.Environment("", "Regex")
     env.id = new remapper.Regex("PlayersPlace").separate().add("Mirror").end(),
     env.scale = [3000, 0.1, 3000];
