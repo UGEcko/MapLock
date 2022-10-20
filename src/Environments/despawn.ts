651cdb77e8c15c8af2ae7d1821d27f5af961b78e
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-20 15:55:15 -0700

    fixed despawn array edition

--- src/Environments/despawn.ts
+++ src/Environments/despawn.ts
@@ -4,13 +4,14 @@ import * as rm from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
 /**
  * @param ID the environment ID to despawn
  * @param lookupmethod the method to find the Environment peice to despawn
- * @author splashcard__ Hi there 
+ * @author splashcard__  || Hi there 
  */
+
 // deno-lint-ignore no-explicit-any
-export function despawn(lookupmethod: any, id: Array<string>,) {
-  id.forEach((object) => {
-        const remove = new rm.Environment(object, lookupmethod)
+export function despawn(lookupmethod: any, id: Array<string>,){
+    id.forEach((object) =>{
+        const remove = new rm.Environment(object, lookupmethod);
         remove.position = [-69420,-69420,-69420];
         remove.push();
-    });
+    })
 }
