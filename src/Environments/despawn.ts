4e53847ecb1329db83a94488cab4288bf390ebda
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-19 18:48:44 -0700

    new despawn function

--- src/Environments/despawn.ts
+++ src/Environments/despawn.ts
@@ -9,17 +9,27 @@ import * as remapper from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
 
 // deno-lint-ignore no-explicit-any
 export function despawn(ID: string, lookupmethod: any) { 
-    switch(ID) {
-        case "bts":
-            ID = "BTSEnvironment.[0]Environment"
-        // deno-lint-ignore no-fallthrough
-        case "billie":
-            ID = "BillieEnvironment.[0]Environment"
-        case "all":
-            ID = "Environment"
-    }
+    
 
     const env = new remapper.Environment(`${ID}`, lookupmethod);
     env.position = [0, -9999, 0];
     env.push();
+}
+
+export function despawn(lookupmethod: any, id: Array<string>,){
+    switch(id) {
+        case "bts":
+            id = "BTSEnvironment.[0]Environment"
+        // deno-lint-ignore no-fallthrough
+        case "billie":
+            id "BillieEnvironment.[0]Environment"
+        case "all":
+            id = "Environment"
+    }
+    
+    id.forEach((env) =>{
+        const remove = new remapper.Environment(env, lookupmethod);
+        remove.position = [-69420,-69420,-69420];
+        remove.push();
+    })
 }
