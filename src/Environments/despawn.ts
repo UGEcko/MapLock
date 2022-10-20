44881c64d17a4419a2724331d86526dc3ca3cdd9
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-20 15:07:06 -0700

    no more cases ;(

--- src/Environments/despawn.ts
+++ src/Environments/despawn.ts
@@ -8,8 +8,6 @@ import * as rm from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
  */
 // deno-lint-ignore no-explicit-any
 export function despawn(lookupmethod: any, id: Array<string>,) {
-    let id = "Environment";
-    
   id.forEach((object) => {
         const remove = new rm.Environment(object, lookupmethod)
         remove.position = [-69420,-69420,-69420];
