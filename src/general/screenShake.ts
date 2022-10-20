a74ea2cdfaf659e7e42404b86f8be43541359fa4
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-20 12:56:02 -0700

    change the right import lol

--- src/general/screenShake.ts
+++ src/general/screenShake.ts
@@ -1,4 +1,4 @@
-import * as remapper from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
+import * as rm from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
 
 /**
 * @param time the time to start the screen shake
