2ca931e68a5cfe15eb2c51b22af8b49312e168c6
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-20 13:03:49 -0700

    Update despawn.ts

--- src/Environments/despawn.ts
+++ src/Environments/despawn.ts
@@ -6,16 +6,7 @@ import * as rm from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
  * @param lookupmethod the method to find the Environment peice to despawn
  * @author splashcard__ Hi there 
  */
-
 // deno-lint-ignore no-explicit-any
-export function despawn(ID: string, lookupmethod: any) { 
-    
-
-    const env = new rm.Environment(`${ID}`, lookupmethod);
-    env.position = [0, -9999, 0];
-    env.push();
-}
-
 export function despawn(lookupmethod: any, id: Array<string>,){
     switch(id) {
         case "bts":
@@ -27,7 +18,7 @@ export function despawn(lookupmethod: any, id: Array<string>,){
             id = "Environment"
     }
     
-  id.forEach((object) =>{
+  id.forEach((object) => {
         const remove = new rm.Environment(object, lookupmethod);
         remove.position = [-69420,-69420,-69420];
         remove.push();
