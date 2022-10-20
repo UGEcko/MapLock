f02f37aaa99293e0f33c36f7334ea9ceadaf9676
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-19 17:51:40 -0700

    Update noteFilter.ts

--- src/general/noteFilter.ts
+++ src/general/noteFilter.ts
@@ -5,10 +5,10 @@ import * as rm from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
 * @param timeStart the time your track will start
 * @param Duration the duration of your track
 * @param position the position to select
-* @param custom data just format this like you would normal noolde scripting
+* @param customData just format this like you would normal noolde scripting
 */
 
-export function noteFilter(timeStart: number, Duration: number, position: array<number>,  customData: any) {
+export function noteFilter(timeStart: number, Duration: number, position: any, customData: any) {
     rm.notesBetween(timeStart, Duration, (note) => {
         if(note.position === position) {
             note.customData = customData;
