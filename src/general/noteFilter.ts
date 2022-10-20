a889cff98fab9f5287038a85618963b392e6a38d
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-19 19:06:27 -0700

    new note filter

--- src/general/noteFilter.ts
+++ src/general/noteFilter.ts
@@ -1,17 +1,20 @@
-// deno-lint-ignore-file no-explicit-any
 import * as rm from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
 
 /**
 * @param timeStart the time your track will start
 * @param Duration the duration of your track
 * @param position the position to select
-* @param customData just format this like you would normal noolde scripting
+* @param customData just format this like you would normal noodle scripting
+* @author splashcard__ <-- bro that guy is like famous
 */
 
-export function noteFilter(timeStart: number, Duration: number, position: any, customData: any) {
-    rm.notesBetween(timeStart, Duration, (note) => {
-        if(note.position === position) {
-            note.customData = customData;
-        }
+// deno-lint-ignore no-explicit-any
+function noteFilter(timeStart: number, duration: number, customData: any, positions: any) {
+    rm.notesBetween(timeStart, duration, (note) => {
+        positions.forEach((position: [number,number, (boolean|undefined)?]) => {
+            if(note.position === position) {
+                note.customData = customData
+            }
+        })
     })
 }
