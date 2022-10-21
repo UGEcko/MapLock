97de51924c248c6a7ed027610fee56554f64e39e
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-21 14:59:25 -0700

    added rm. lol

--- src/Environments/sun.ts
+++ src/Environments/sun.ts
@@ -4,13 +4,13 @@ import * as rm from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
  * @param postion position to spawn the sun
  * @param scale the scale of the sun
  * @param lightID the ID to light the sun
- * @author splashcard__ respect the grind
+ * @author splashcard__ 
  */
 
 export function sun(position: rm.KeyframesAny, scale: number, lightID : number, environment: string) {
     switch(environment){
         case: "billie":
-            const sun = new Environment("BillieEnvironment.[0]Environment.[27]DayAndNight.[0]Day.[0]Sun", "Exact")
+            const sun = new rm.Environment("BillieEnvironment.[0]Environment.[27]DayAndNight.[0]Day.[0]Sun", "Exact")
             sun.position = position; 
             sun.scale = [scale, scale, scale];
             sun.push();
