d67153a8d395c0fe26f30024235888b75100f5c7
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-21 12:33:27 -0700

    Update LRnotes.ts

--- src/general/LRnotes.ts
+++ src/general/LRnotes.ts
@@ -9,10 +9,10 @@ import * as rm from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
 // deno-lint-ignore no-explicit-any
 export function LRnotes(time: number, duration: number, leftNoteData: any, rightNoteData: any) {
   rm.notesBetween(time, duration, (note) => {
-    if(note.position === [0, 0] || [0, 1] || [0, 2] || [1, 0] || [1, 1] || [1, 2]) {
+    if(note.type === 0) {
       note.customData = leftNoteData;
     }
-    if((note.position === [2, 0] || [2, 1] || [2, 2] || [3, 0] || [3, 1] || [3, 2])) {
+    if((note.type === 1) {
       note.customData = rightNoteData;
     }
   })
