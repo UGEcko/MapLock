4bbf2246fed85baf95b6ee401f3f0e7de1ac0668
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-21 15:03:36 -0700

    added mountain function idk

---
+++ src/Environments/mountains.ts
@@ -0,0 +1,14 @@
+import * as rm from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
+
+/**
+* @param height the heght to spawn the mountains at
+* @param rotation the y axis rotation of the mountains
+* @author splashcard__ || these are just getting less and less original
+*/
+
+export function mountains(height: number, rotation: number) {
+    const mtn = new Environment("BillieEnvironment.[0]Environment.[12]Mountains", "Exact");
+    mtn.position = [0, height, 0];
+    mtn.rotation = [0, rotation, 0];
+    mtn.push();
+}
