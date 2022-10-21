5a88e8d427bbe2fa2187c15a069c6c26a99d0bc6
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-21 14:58:04 -0700

    added billie env support :)

--- src/Environments/sun.ts
+++ src/Environments/sun.ts
@@ -7,15 +7,26 @@ import * as rm from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
  * @author splashcard__ respect the grind
  */
 
-export function sun(position: rm.KeyframesAny, scale: number, lightID : number) {
-    const sun = new rm.Geometry("Cylinder", {
-        _shader: "OpaqueLight"
-    })
-    sun.position = position;
-    sun.scale = [scale, scale, 0.3];
-    sun.lightID = lightID;
-    sun.rotation = [90, 0, 0]
-    sun.push();
+export function sun(position: rm.KeyframesAny, scale: number, lightID : number, environment: string) {
+    switch(environment){
+        case: "billie":
+            const sun = new Environment("BillieEnvironment.[0]Environment.[27]DayAndNight.[0]Day.[0]Sun", "Exact")
+            sun.position = position; 
+            sun.scale = [scale, scale, scale];
+            sun.push();
+           
+        case: "universal":
+                const sun = new rm.Geometry("Cylinder", {
+                _shader: "OpaqueLight"
+                })
+                sun.position = position;
+                sun.scale = [scale, scale, 0.3];
+                sun.lightID = lightID;
+                sun.rotation = [90, 0, 0];
+                sun.push();
+        }
+    
+ 
 }
 
 
