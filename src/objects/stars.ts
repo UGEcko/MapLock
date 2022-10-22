1fa183159eeced28aba1781772eed9487df51dcf
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-22 12:04:29 -0700

    Update stars.ts

--- src/objects/stars.ts
+++ src/objects/stars.ts
@@ -1,7 +1,7 @@
 import * as rm  from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
 
 /**
-* @param ammount the ammount of stars to spawn
+* @param amount the ammount of stars to spawn
 * @param individualSize the size of each star
 * @param Distance the distance between each star
 * @param Time the time to spawn the stars
@@ -16,7 +16,7 @@ export function stars(amount: number, individualSize: number, Distance: number, 
       const starRotation: rm.Vec3 = [rm.rand(10, 360), rm.rand(10, 360), 0];
     
       stars.animate.definitePosition = rm.arrAdd(rm.rotatePoint(starRotation, [0, Distance, -individualSize / 2]), 0) as rm.Vec3;
-      stars.color = [1,1,1,10];
+      stars.color = [3, 3, 3, 10];
       stars.scale = [individualSize, individualSize, individualSize];
       stars.fake = true;
       stars.interactable = false;
