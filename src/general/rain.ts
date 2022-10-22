a5e4ba72f64312f640e569cf43b3271e87664fe1
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-22 11:42:21 -0700

    fixed the goof

--- src/general/rain.ts
+++ src/general/rain.ts
@@ -1,7 +1,7 @@
 import * as rm from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
 
 /**
-* unfortunaltely specific to billie envieonment D:
+* unfortunaltely specific to billie environment D:
 * @param lightID the id so you can light up the rain, why does this work? idk
 * @author splashcard__
 */
