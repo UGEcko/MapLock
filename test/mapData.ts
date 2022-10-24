e1dba43492289783e9c367a6f788bdbba3ad323d
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-24 14:37:45 -0700

    njs map data tm

--- test/mapData.ts
+++ test/mapData.ts
@@ -1,20 +1,23 @@
 import * as rm from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
 
 export function mapData() {
-    rm.notesBetween(0, 9999999, (note), (wall) => {
-      console.log(`\x1b[36m%s\x1b[0m`, //start label
+    console.log(`\x1b[36m%s\x1b[0m`, //start label
       `=================================================MAP DATA=============================================================`
-)
+        )
+    rm.notesBetween(0, 9999999, (note) => {
       console.log(`\x1b[36m%s\x1b[0m`, //note label
       `=================================================NOTE DATA=============================================================`
-)
-      console.log(`\x1b[36m%s\x1b[0m`, `note NJS: ${note.njs}`);//njs
-      console.log(`\x1b[36m%s\x1b[0m`, //wall label
+        )
+      console.log(`\x1b[36m%s\x1b[0m`, `note NJS: ${note.njs}`);//note njs
+        
+        )
+  })
+    rm.wallsBetween(0, 99999999, (wall) => {
+              console.log(`\x1b[36m%s\x1b[0m`, //wall label
       `=================================================WALL DATA=============================================================`
 )
       console.log(`\x1b[36m%s\x1b[0m`, `wall NJS: ${wall.njs}`
-      console.log(`\x1b[36m%s\x1b[0m`, //end label
-      `=================================================MAP DATA=============================================================`
-)
-  })
+    }
+          console.log(`\x1b[36m%s\x1b[0m`, //end label
+      `=================================================MAP DATA=============================================================`)
 }
