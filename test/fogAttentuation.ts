7de883689a65a08a9aa8f127c25ccaafd3912b3d
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-24 12:24:25 -0700

    testing this ig

---
+++ test/fogAttentuation.ts
@@ -0,0 +1,16 @@
+import * as rm from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
+
+/**
+* massively stupid way to animate attentuation of fog lol
+* @param time the time to start the fog animation
+* @param duration the duration of the fog animtion
+* @param animateAttentuation the attentuation animataion of the fog
+* @author splashcard__ < morinin
+*/
+
+export function fogAttentuation(time: number, duration: number, animateAttentuation: KeyframesLinear) {
+    const fog = new CustomEvent(time).animateTrack("fog");
+    fog.animate.attenuation = animateAttentuation;
+    animTrack.duration = duration;
+    animTrack.push();
+}
