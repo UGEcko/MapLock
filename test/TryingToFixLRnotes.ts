ca4bef6de17b7bb0d534ed70310316e735d2819f
Splashcard04 <111317032+Splashcard04@users.noreply.github.com>
2022-10-26 12:35:30 -0700

    trying to fix LR notes lol

---
+++ test/TryingToFixLRnotes.ts
@@ -0,0 +1,18 @@
+import { notesBetween } from "https://deno.land/x/remapper@2.1.0/src/mod.ts"
+
+/**
+* uses an end time, NOT A DURATION
+* @param timeStart
+* @param timeEnd
+* @param leftData
+* @param rightData
+*/
+
+export function LRnotes(timeStart, TimeEnd, leftData: any, rightData: any) {
+  //i have no clue if this works
+  if(note.type === 0, (leftNote => {
+    leftNote.customData = leftData
+  });
+  if(note.type === 1, (rightNote => {
+    rightNote.customData = rightData
+  })
